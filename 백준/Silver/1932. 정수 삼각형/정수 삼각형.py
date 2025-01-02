d722c1a17e8f12a4d21e22fbaf59560ed44e0f7b
osrri <105829324+irrso@users.noreply.github.com>
2025-01-02 00:40:18 -0800

    [Silver I] Title: 정수 삼각형, Time: 124 ms, Memory: 41524 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,30 +1,16 @@
 # input
 n = int(input())
-tri = []
-for _ in range(n):
-	tri += list(map(int, input().split()))
+tri = [list(map(int, input().split())) for _ in range(n)]
 
 # solve
-dp = [0]*(sum(range(1, n+1)))
-dp[0] = tri[0]
+dp = [[0]*(i+1) for i in range(n)]
+dp[0][0] = tri[0][0]
 
-idx = 0
 for i in range(1, n):
-	prev = idx
-	idx += i
-	dp[idx] = dp[prev] + tri[idx]
-
-idx = 0
-for i in range(2, n+1):
-	prev = idx
-	idx += i
-	dp[idx] = dp[prev] + tri[idx]
-
-idx = 4
-for i in range(3, n+1):
-	for j in range(i-2):
-		dp[idx+j] = max(dp[idx+j-i], dp[idx+j-i+1]) + tri[idx+j]
-
-	idx += i
-
-print(max(dp[-n:]))
+	dp[i][0] = dp[i-1][0] + tri[i][0]
+	dp[i][-1] = dp[i-1][-1] + tri[i][-1]
+	
+	for j in range(1, i):
+		dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + tri[i][j]
+	
+print(max(dp[-1]))
