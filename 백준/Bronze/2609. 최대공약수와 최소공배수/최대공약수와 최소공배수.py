4c62e94c505766e95bd3421cbea56309ae7ee9fd
osrri <105829324+irrso@users.noreply.github.com>
2025-01-08 21:01:32 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -2,21 +2,13 @@
 A, B = map(int, input().split())
 
 # solve
-def gcd(A, B):
+def solve(A, B):
 	for i in range(A, 0, -1):
 		if A%i == 0 and B%i == 0:
 			print(i)
+			print(B*(A//i))
 			break
-
-def lcm(A, B):
-	i = 1
-	while(True):
-		if (A*i)%B == 0:
-			print(A*i)
-			break
-		i += 1
 
 A, B =  max(A, B), min(A, B)
 
-gcd(A, B)
-lcm(A, B)
+solve(A, B)
