123eda75d862f4bbaca0208ee62d9334e1fb9495
osrri <105829324+irrso@users.noreply.github.com>
2025-05-07 20:57:05 +0900

    [Gold IV] Title: 테트로미노, Time: 4944 ms, Memory: 39920 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,37 +1,53 @@
-from collections import deque
-
 # input
 N, M = map(int, input().split())
 paper = [list(map(int, input().split())) for _ in range(N)]
 
 # solve
-dir = [[(0, 0), (0, 1), (0, 2), (0, 3)], [(0, 0), (1, 0), (2, 0), (3, 0)], # ㅡ
-
-		[(0, 0), (0, 1), (1, 0), (1, 1)], # ㅁ
-
-		[(0, 0), (1, 0), (2, 0), (2, 1)], [(0, 0), (1, 0), (2, 0), (2, -1)], # L
-		[(0, 0), (0, 1), (1, 1), (2, 1)], [(0, 0), (0, -1), (1, -1), (2, -1)],
-		[(0, 0), (1, 0), (1, 1), (1, 2)], [(0, 0), (1, 0), (1, -1), (1, -2)],
-		[(0, 0), (0, 1), (0, 2), (1, 2)], [(0, 0), (0, -1), (0, -2), (1, -2)],
-		
-		[(0, 0), (0, 1), (0, 2), (1, 1)], [(0, 0), (0, 1), (0, 2), (-1, 1)], # ㅜ
-		[(0, 0), (0, 1), (-1, 1), (1, 1)], [(0, 0), (0, -1), (-1, -1), (1, -1)],
-
-		[(0, 0), (1, 0), (1, 1), (2, 1)], [(0, 0), (1, 0), (1, -1), (2, -1)], # ㄱㅡ
-		[(0, 0), (0, -1), (1, -1), (1, -2)], [(0, 0), (0, 1), (1, 1), (1, 2)]]
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
 
 ans = 0
+visited = [[False]*M for _ in range(N)]
+
+def dfs(i, j, tmp, cnt):
+	global ans, visited
+
+	if cnt == 4:
+		ans = max(ans, tmp)
+		return
+
+	for y, x in zip(dy, dx):
+		ny = i+y
+		nx = j+x
+		if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx]:
+			visited[ny][nx] = True
+			dfs(ny, nx, tmp+paper[ny][nx], cnt+1)
+			visited[ny][nx] = False
+
+def other(i, j):
+	global ans, visited
+
+	dir = [[(0, 0), (0, 1), (0, 2), (1, 1)], [(0, 0), (0, 1), (0, 2), (-1, 1)],
+			[(0, 0), (0, 1), (-1, 1), (1, 1)], [(0, 0), (0, -1), (-1, -1), (1, -1)]]
+
+	for d in dir:
+		tmp = 0
+		for dy, dx in d:
+			ny = i+dy
+			nx = j+dx
+			if 0 <= ny < N and 0 <= nx < M:
+				tmp += paper[ny][nx]
+			else:
+				break
+
+		ans = max(ans, tmp)
+
+
 for i in range(N):
 	for j in range(M):
-		for d in dir:
-			temp = 0
-			for dy, dx in d:
-				ny = i+dy
-				nx = j+dx
-				if 0 <= ny < N and 0 <= nx < M:
-					temp += paper[ny][nx]
-				else:
-					break
-			ans = max(ans, temp)
+		visited[i][j] = True
+		dfs(i, j, paper[i][j], 1)
+		visited[i][j] = False
+		other(i, j)
 
 print(ans)
