7ec9cb69588f8636dd053c1fa33cf347d67d95f3
osrri <105829324+irrso@users.noreply.github.com>
2026-07-21 17:54:44 +0900

    [level 3] Title: N으로 표현, Time: 3.26 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,26 +1,19 @@
+from collections import deque
+from itertools import product
+
 def solution(N, number):
-	dp = [[int(str(N)*i) if i else 0] for i in range(9)]
+    dp = [{int(str(N)*i) if i else 0} for i in range(9)]
 
-	for i in range(1, 9):
-		result = set()
+    for n in range(1, 9):
+        for i in range(1, n//2+1):
+            for n1, n2 in product(dp[i], dp[n-i]):
+                dp[n].add(n1+n2)
+                dp[n].add(n1*n2)
+                dp[n].add(max(n1//n2, n2//n1))
+                if n1-n2 != 0 or n2-n1 != 0:
+                    dp[n].add(max(n1-n2, n2-n1))
 
-		if dp[i][0] == number:
-			return i
-
-		for j in range(1, i):
-			for n1 in dp[j]:
-				for n2 in dp[i-j]:
-					dp[i].append(n1+n2)
-
-					if n1-n2 > 0:
-						dp[i].append(n1-n2)
-
-					dp[i].append(n1*n2)
-
-					if n1//n2 > 0:
-						dp[i].append(n1//n2)
-
-		if number in dp[i]:
-			return i
-
-	return -1
+        if number in dp[n]:
+            return n
+    
+    return -1
