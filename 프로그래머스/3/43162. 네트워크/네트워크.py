6402792728932dc886d491b5bdbaf5b24e8e97e5
osrri <105829324+irrso@users.noreply.github.com>
2026-07-24 22:25:10 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,30 @@
-def dfs(node, visited, n, computers):
-	visited[node] = True
-
-	for i in range(n):
-		if computers[node][i] and not visited[i]:
-			dfs(i, visited, n, computers)
-
-
 def solution(n, computers):
-	ans = 0
-	visited = [False]*n
+    answer = 0
+    visited = [False]*n
+    
+    for node in range(n):
+        if not visited[node]:
+            dfs(node, visited, n, computers)
+            answer += 1
 
-	for i in range(n):
-		if not visited[i]:
-			dfs(i, visited, n, computers)
-			ans += 1
+    return answer
 
-	return ans
+
+def dfs(node, visited, n, computers):
+    visited[node] = True
+    
+    for nxt_node in range(n):
+        if computers[node][nxt_node]==1 and not visited[nxt_node]:
+            dfs(nxt_node, visited, n, computers)
+
+
+'''
+네트워크의 개수
+- A와 B연결, B와 C연결 -> A와 C연결 -> A, B, C는 같은 네트워크
+
+dfs
+
+n: 컴퓨터 개수 1~200
+각 컴퓨터: 0~(n-1)
+computers[i][j]: 1이면 i, j번 컴퓨터 연결
+'''
